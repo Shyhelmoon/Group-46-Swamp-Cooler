09cf8f1d7412e3bed236daf8c17b0d4cff654cd8
Simeon Dimitrov <113215857+sdimitrov164@users.noreply.github.com>
2024-12-13 15:13:35 -0800

    Setting up main.cpp
    Addeed header files, necessary libraries.
    Setup the pins for the arduino and the modules that we will be using.

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,116 @@
+#include "myArduino.h" // Contains our register-level versions of common Arduino functions
+#include <Stepper.h> // Stepper library, provided by Adafruit, for stepper motor control
+#include <RTClib.h> // RTC library, provided by Adafruit, for clock module
+#include <SPI.h> // Needed so RTClib.h can work
+#include <DHT.h> // DHT library, provided by Adafruit, for temp and humidity sensor
+#include <Adafruit_Sensor.h> //Needed so DHT can work
+#include <LiquidCrystal.h> // LCD display library
 
+// ADC, Timer, UART, and other functions that are typically available in the Arduino library
+// that we must recreate can be found in myArduino.h. They have been seperated for clarity within the main file.  
+
+//Definitions and constraints
+// Define variable to track state.
+enum state{IDLE, RUNNING, DISABLED, ERROR};
+state currentState = DISABLED;
+state previousState = DISABLED;
+
+//Define variables for switching states
+float currentTemp = 0;
+float currentWaterLevel = 0;
+const int WATER_THRESHOLD = 110; //Level of our reservoir threshold
+const float TEMP_THRESHOLD = 30.00; //DHT Sensor is slightly skewed
+
+//Configure on/off button
+const int START_DIGITAL_PIN = 18; //For attachInterrupt()
+const int START_PIN = PD3;
+volatile bool on = false;
+
+// Define state LED GPIO constants
+const int LED_IDLE = PB3; // D50
+const int LED_RUNNING = PB2; // D51
+const int LED_DISABLED = PB1; // D52
+const int LED_ERROR = PB0; // D53
+
+// Configure vent fan, vent fan control, and main fan
+const int stepsPerRevolution = 2038;
+Stepper vent = Stepper(stepsPerRevolution, 23, 27, 25, 29); // IN1 = 23, IN2 = 25, IN3 = 27, IN4 = 29
+int currentStepperPos = 0, previousVentPos = 0;
+const int VENT_POT_PIN = 0; // ADC channel 0
+const int FAN_PIN = PC2; //D35
+
+//Configure clock, temp and humidity sensor, water level sensor 
+RTC_DS1307 clock;
+const int DHT_PIN = 45;
+DHT dht(DHT_PIN, DHT11);
+const int WATER_LVL_PIN = 1; // ADC channel 1
+
+//Configure LCD pins and variables for LCD delay
+const int RS = 12, EN = 11, D4 = 5, D5 = 4, D6 = 3, D7 = 2;
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+unsigned long previousMillis = 0, currentMillis = 0;
+const unsigned long updateDisplayInterval = 60000; //60000 ms = 1 minute
+
+// ==== FUNCTION PROTOTYPES ====
+//Setup functions
+void gpioInit();
+void sensorsMotorsInit();
+
+//State control functions
+void toggleOn();
+void checkState();
+void updateLED();
+
+//Vent control functions
+void controlVent();
+void printVentPosition(int newpos);
+
+//Event reporting functions
+void printCurrentTime();
+void printStateChange();
+
+//Display temperature functions
+void displayTempAndHumidity();
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// Function Definitions
+void gpioInit() {
+    //Set all LEDs to output, then all to LOW
+    DDRB |= (1 << LED_IDLE) | (1 << LED_RUNNING) | (1 << LED_DISABLED) | (1 << LED_DISABLED);
+    PORTB &= ~((1 << LED_IDLE) | (1 << LED_RUNNING) | (1 << LED_DISABLED) | (1 << LED_ERROR));
+
+    // Setup motor as output, initialize to LOW to disable fan
+    DDRC |= (1 << FAN_PIN);
+    PORTC &= ~(1 << FAN_PIN);
+
+    // Setup on/off button as input w_pullup.
+    DDRD &= ~(1 << START_PIN);
+    PORTD |= (1 << START_PIN);
+    attachInterrupt(digitalPinToInterrupt(START_DIGITAL_PIN), toggleOn, FALLING);
+}
+void sensorsMotorsInit() {
+    // Begin RTC module
+    if (!clock.begin()) {
+        U0printString("Couldn't find clock");
+        while (1);
+    }
+
+    // Begin DHT module
+    dht.begin();
+
+    // Water level sensor needs just simple ADC read, no setup needed
+
+    // Setup vent speed
+    vent.setSpeed(10);
+}
+
+void toggleOn() {
+    on = !on;
+}
